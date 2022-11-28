d0ae86246bc1fafc90acfb59cb3f71751cfc0abc
mukesh <mukeshkumar42000000@gmail.com>
2022-11-28 16:52:38 +0530

    some

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -4,15 +4,16 @@ function Hero() {
   return (
     <>
       <div className='hero'>
-        <div>
+        <div className=''>
           <h1>Hi, I'M <br />Alika Maya</h1>
           <p>A Photographer & Business Strategist who empowers<br />entrepreneurs to build a brand, market it on social<br />media, and create a life they love.</p>
           <div>
             <input type="text" /> <span><button></button>Join with me now</span>
           </div>
         </div>
-        <div>
-        <Image src=""></Image>
+        <div className='hero_bg'>
+        
+        {/* <Image src="/Images/Hero.png"  height={250} width={200} className="hero_img" alt=""></Image> */}
         </div>
       </div>
     </>
