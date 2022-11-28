2f8f20ffe4126d54062ad9f39a1dd454fb326145
Mukesh <mukeshkumar42000000@gmail.com>
2022-11-28 22:16:23 +0530

    hero section

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -4,16 +4,17 @@ function Hero() {
   return (
     <>
       <div className='hero'>
-        <div className=''>
+        <div className='hero_content'>
           <h1>Hi, I'M <br />Alika Maya</h1>
           <p>A Photographer & Business Strategist who empowers<br />entrepreneurs to build a brand, market it on social<br />media, and create a life they love.</p>
           <div>
-            <input type="text" /> <span><button></button>Join with me now</span>
+            <input type="text" placeholder='Enter Your Email Address'/> <button>Join with me now</button>
           </div>
         </div>
         <div className='hero_bg'>
         
-        {/* <Image src="/Images/Hero.png"  height={250} width={200} className="hero_img" alt=""></Image> */}
+        <Image src="/Images/background.png" height={600} width={500} className="hero_img" alt=""></Image>
+
         </div>
       </div>
     </>
