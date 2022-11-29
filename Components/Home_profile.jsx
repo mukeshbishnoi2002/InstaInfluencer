6e97cde65a5cfd8d8b5e51713d9f7ab5ac45c7e9
mukesh <mukeshkumar42000000@gmail.com>
2022-11-29 17:14:01 +0530

    some more css

--- Components/Home_profile.jsx
+++ Components/Home_profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Hero from './Hero'
+import Influencer_info from './Influencer_info';
 function Home() {
   return (
    <>
         <Header />
         <Hero />
+        <Influencer_info />
    </>
   )
 }
--- Components/Influencer_info.jsx
+++ Components/Influencer_info.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
-
+import Image from 'next/image';
+import { influencer_information } from '../Utility/Cards'
 function Influencer_info() {
   return (
-    <div className='influencer_info'>
-        <p>WHAT DO I LOVE</p>
-            <h1>I'm a Instagram influencer designer running my own design studio</h1>
-            <div className='influencer_card'>
-                 <div className='card_one card'>
-                        
-                 </div>
-                 <div className='card_two card'>
+  <div className='influencer'>
+     <div className='influencer_info'>
+     <p className='what_do'>WHAT DO I LOVE</p>
+     <h1 className='influencer_info_heading'>I'm a Instagram influencer designer running my own design studio</h1>
+     <div className='influencer_card'>
+       {
+         influencer_information.map((items, index) => {
+           const { id, suscriber, platform, icon, influencer_nanme, dec } = items;
+           return (
+             <div className='card' key={id}>
+             
+               <p className='suscriber'>{suscriber}K</p>
+               <p className='platform'>{platform} followers</p>
+               <i class={icon}></i>
+               <span> @{influencer_nanme}</span>
+               <p className='dec'>{dec}</p>
+               <div className='effect'></div>
+             </div>
+           )
+         })
+       }
+      
+     </div>
+     <Image src="/Images/insta-shape1.png" width={400} height={400} className="cards_background_effect"></Image>
+   </div>
 
-                 </div>
-                 <div className='card_three card'>
-
-                 </div>
-            </div>
-    </div>
+  </div>
   )
 }
 
