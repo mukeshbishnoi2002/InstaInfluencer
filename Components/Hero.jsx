a2a228ec7718b6124844cda574c4a4345a65dafa
Mukesh <mukeshkumar42000000@gmail.com>
2022-11-29 11:36:34 +0530

    hero css width set

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -5,15 +5,15 @@ function Hero() {
     <>
       <div className='hero'>
         <div className='hero_content'>
-          <h1>Hi, I'M <br />Alika Maya</h1>
-          <p>A Photographer & Business Strategist who empowers<br />entrepreneurs to build a brand, market it on social<br />media, and create a life they love.</p>
+          <h1>Hi, I'M Alika Maya</h1>
+          <p>A Photographer & Business Strategist who empowers entrepreneurs to build a brand, market it on social media, and create a life they love.</p>
           <div>
             <input type="text" placeholder='Enter Your Email Address'/> <button>Join with me now</button>
           </div>
         </div>
         <div className='hero_bg'>
         
-        <Image src="/Images/background.png" height={600} width={500} className="hero_img" alt=""></Image>
+        <Image src="/Images/background.png" height={610} width={480} className="hero_im" alt=""></Image>
 
         </div>
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import '../styles/Large.css'
+import '../styles/Medium.css'
+import '../styles/Small.css'
+import '../styles/Extra_small.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
