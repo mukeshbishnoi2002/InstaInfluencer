1a5d8670f52f3501e43bd2ec12a8b1d0599ef71f
Mukesh <mukeshkumar42000000@gmail.com>
2022-11-30 08:15:08 +0530

    responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/Large.css'
 import '../styles/Medium.css'
 import '../styles/Small.css'
 import '../styles/Extra_small.css'
+import '../styles/Six_hundred.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
